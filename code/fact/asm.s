c6b9d149460a511a7f8782045b4edd07d477db65
COQUARD Cyrille <coquardcyr@eisti.eu>
2020-01-28 10:41:38 +0100

    Added factorial

--- code/fact/asm.s
+++ code/fact/asm.s
@@ -1,20 +1,28 @@
-MOV r2, 0
 MOV r3, 0
-CMP r0, 0
-BNQ 2
+MOV r4, 0
+MOV r5, 0
+MOV r6, r0
+MOV r3, r0
+MOV r0, r3
+SUB r6, r6, 1
+MOV r1, r6
+MOV r5, r4
+MOV r3, 0
+MOV r4, 0
+CMP r6, 0
+BEQ 17
 CMP r1, 0
-BEQ 14
-MOV r4, r0
-MOV r5, r1
-CMP r4, 0
-BNQ 2
-CMP r5, 0
-BEQ 4
-ADC r2,r2, r4
-ADC r3, r3, r5
-SUB r4, r4, 1
-SUB r5, r5, 0
-B -8
-SUB r0, r0, 1
-SUB r1, r1, 0
-B -17
+BEQ 11
+LSH r7, r1, 63
+RSH r7, r7, 63
+CMP r7, 1
+BNE 3
+ADC r3, r3, r0
+ADC r4, r4, r5
+LSH r0, r0, 1
+LSH r5, r5, 1
+RSH r1, r1, 1
+B -11
+MOV r8, r3
+MOV r9, r4
+B -22
